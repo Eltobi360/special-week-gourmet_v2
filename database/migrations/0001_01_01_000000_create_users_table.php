b88c16c347cb6dc5b06796be8a83bada9da9ee69
Jorge <mneyraarzapalo@gmail.com>
2025-09-23 18:38:41 -0500

    creacion de brezee + stapie y la configuracion de incio de secion

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('telefono', 50)->nullable();   // <-- agregado
+            $table->boolean('activo')->default(true);
             $table->rememberToken();
             $table->timestamps();
         });
